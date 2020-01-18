2c41dc1071791f02c79b119b752d609f434d1b26
pauldub04 <paul.dubnyakov@gmail.com>
2020-01-18 15:04:11 +0300

    added nav bar++

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+      <v-navigation-drawer color="green"
+                           dark
+                           expand-on-hover
+                           hide-overlay
+                           permanent
+                           right
+                           app>
+        <v-list
+            nav 
+            shaped
+            dense
+        >
+          <v-list-item two-line>
+            <v-list-item-avatar>
+              <img :src="'https://randomuser.me/api/portraits/men/1.jpg'">
+            </v-list-item-avatar>
+
+            <v-list-item-content class="text-left">
+              <v-item-title class="font-weight-black">SocialLink</v-item-title>
+              <v-item-subtitle>Иван Иванов</v-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider class="my-3"></v-divider>
+
+          <v-list-item link v-for="link in links" :key="link.link" :to="link.path">
+            <v-list-item-icon>
+              <v-icon>{{link.icon}}</v-icon>
+            </v-list-item-icon> 
+
+            <v-list-item-content>
+              <v-item-title class="text-left">{{link.label}}</v-item-title>
+            </v-list-item-content>       
+          </v-list-item>
+        </v-list>
+
+      </v-navigation-drawer>
+    
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+      links: [
+        {
+          label: "Главная",
+          path: "/",
+          icon: "mdi-home-outline"
+        },
+        {
+          label: "Профиль",
+          path: "/profile",
+          icon: "mdi-account"
+        }
+      ]
+  })
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
